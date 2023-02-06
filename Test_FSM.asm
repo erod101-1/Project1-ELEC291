eeab2f4b324f9ddabf0ff73940f8a59f60a2a2f9
AbrahamBahrami <124313374+AbrahamBahrami@users.noreply.github.com>
2023-02-06 13:30:28 -0800

    Update Test_FSM.asm

--- Test_FSM.asm
+++ Test_FSM.asm
@@ -6,33 +6,33 @@ Temp_refl: ds 1
 Time_refl: ds 1
 
 state0:
-cjne a, #0, state1
-mov pwm, #0
-jb PB6, state0_done
-jnb PB6, $ ; Wait for key release
-mov state, #1
-state0_done:
-ljmp forever
+  cjne a, #0, state1
+  mov pwm, #0
+  jb PB6, state0_done
+  jnb PB6, $ ; Wait for key release
+  mov state, #1
+  state0_done:
+  ljmp forever
 
 state1:
-cjne a, #1, state2
-mov pwm, #100
-mov sec, #0
-mov a, temp_soak
-clr c
-subb a, temp
-jnc state1_done
-mov state, #2
-state1_done:
-ljmp forever
+  cjne a, #1, state2
+  mov pwm, #100
+  mov sec, #0
+  mov a, temp_soak
+  clr c
+  subb a, temp
+  jnc state1_done
+  mov state, #2
+  state1_done:
+  ljmp forever
 
 state2:
-cjne a, #2, state3
-mov pwm, #20
-mov a, time_soak
-clr c
-subb a, sec
-jnc state2_done
-mov state, #3
-state2_done:
-ljmp forever
+  cjne a, #2, state3
+  mov pwm, #20
+  mov a, time_soak
+  clr c
+  subb a, sec
+  jnc state2_done
+  mov state, #3
+  state2_done:
+  ljmp forever
