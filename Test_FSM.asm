e7d96300b58ce88d0d65d9b188aa95f948f95f53
AlexLamyin <124329681+AlexLamyin@users.noreply.github.com>
2023-02-06 14:02:14 -0800

    Create Test_FSM.asm

--- Test_FSM.asm
+++ Test_FSM.asm
@@ -1,15 +1,16 @@
 DSEG ; Before the state machine!
-state: ds 1
+state:     ds 1
 temp_soak: ds 1
 time_soak: ds 1
 temp_refl: ds 1
 time_refl: ds 1
-temp_cooling: ds 1
+temp_cool: ds 1
 
 // setting parameters with inputs on LCD
 // defining inputs (buttons)
-// second incrementing
-// pwm to SSR 
+// second incrementing (using lab 2 timer?)
+// pwm to SSR (Pulse Width Modulation, 100% for ramping, 20% for keeping constant temp)
+// PS: may need to change to non-volatile memory (EEPROM) to set and save parameters
 
 clr a
 mov state, a
@@ -51,7 +52,33 @@ state2_done:
 state3:
   cjne a, #3, state4
   mov pwm, #100
+  mov sec, #0
   mov a, temp_refl
   clr c
   subb a, temp
+  jnc state3_done
+  mov state, #4
+state3_done:
+  ljmp forever
   
+state4:
+  cjne a, #4, state5
+  mov pwm, #20
+  mov a, time_refl
+  clr c
+  subb a, sec
+  jnc state4_done
+  mov state, #5
+state4_done:
+  ljmp forever
+  
+state5:
+  cjne a, #5, state0
+  mov pwm, #0
+  mov a, temp_cool
+  clr c
+  subb a, temp
+  jc state5_done
+  mov state, #0
+state5_done:
+  ljmp forever
