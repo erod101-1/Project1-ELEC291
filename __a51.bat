8082bf794c5b40e350a45908a84cda35e3c3c9bd
kcgroulx <87883655+kcgroulx@users.noreply.github.com>
2023-03-02 15:58:15 -0800

    Final Commit

--- __a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-C:
-cd "\Users\kcgro\Documents\GitHub\Project1-ELEC291\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\kcgro\Documents\GitHub\Project1-ELEC291\sound_timed.asm"
-echo Crosside_Action Set_Hex_File C:\Users\kcgro\Documents\GitHub\Project1-ELEC291\sound_timed.HEX
